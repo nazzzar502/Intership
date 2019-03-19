a36d30a8ec958c2a16c4cd314d9328ff413622eb
Муляк Назар <nazzzar502@gmail.com>
2019-03-19 16:18:24 +0200

    Intersip class modification

--- Institution/InterLink/Internship.cs
+++ Institution/InterLink/Internship.cs
@@ -1,16 +1,57 @@
+using InternshipTest.Person;
+using System;
 namespace InternshipTest.Institution.InterLink
 {
     public class Internship
     {
+        public Student[] interns = new Student[99];
+        int knowledge_sum = 0;
+
         public Internship(string name)
         {
             //TODO: Implementation is needed      
         }
 
-        public string GetStudents()
+        int count = 0;
+        public void GetStudents(University university, int number)
         {
-            //TODO: Implementation is needed
-            return "Andrew Maslenko\nJulia Veselkina\n";
+            foreach (Student i in university.students)
+            {
+                if (i != null) { knowledge_sum = knowledge_sum + i.student_knowledge; }
+
+            }
+            knowledge_sum = knowledge_sum / number;
+
+
+
+            foreach (Student i in university.students)
+            {
+                count++;
+                if (i != null)
+                {
+                    if (i.student_knowledge > knowledge_sum)
+                    {
+                        interns[count] = i;
+                    }
+                }
+
+
+
+            }
+
+        }
+
+        public void ShowStudents()
+        {
+            foreach (Student i in interns)
+            {
+                if (i != null)
+                {
+                    Console.WriteLine(i.student_name);
+                    Console.WriteLine(i.student_knowledge);
+                }
+
+            }
         }
     }
 }
